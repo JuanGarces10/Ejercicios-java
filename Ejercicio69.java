e5e732b833749b668f6c16c92abbacea9effce46
JuanGarces10 <juan.garcesmoreno@unicolombo.edu.co>
2024-04-22 20:03:08 -0500

    Ejercicio 69

---
+++ Ejercicio69.java
@@ -0,0 +1,9 @@
+public class Ejercicio69 {
+    public static void main(String[] args) {
+        int suma = 0;
+        for (int i = 21; i >= 2; i -= 2) {
+            suma += i;
+        }
+        System.out.println("La suma es: " + suma);
+    }
+}
